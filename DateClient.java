63d07e1840fa37645f276be8db18492fb14e538d
Jyothishma130 <jyothishma130@gmail.com>
2026-02-05 16:52:53 -0600

    Update DateClient.java

--- DateClient.java
+++ DateClient.java
@@ -2,27 +2,31 @@ import java.io.*;
 import java.net.*;
 
 public class DateClient {
-
     public static void main(String[] args) {
-        String serverIP = "172.16.32.102"; 
-        int port = 6013;
-
         try {
-            Socket socket = new Socket(serverIP, port);
-            System.out.println("Connected to DateServer");
+            String serverIP = "172.16.32.102"; 
+            Socket socket = new Socket(serverIP, 6013);
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-            String response;
-            while ((response = in.readLine()) != null) {
-                System.out.println(response);
+            String userMessage;
+            while (true) {
+                System.out.print("Client message: ");
+                userMessage = console.readLine();
+                if (userMessage.equalsIgnoreCase("exit")) break;
+
+                out.println(userMessage);          // send to server
+                String serverResponse = in.readLine();  // read server response
+                System.out.println("Server: " + serverResponse);
             }
 
             socket.close();
+            System.out.println("Disconnected from server.");
 
-        } catch (IOException ioe) {
-            System.err.println(ioe);
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }
 }
